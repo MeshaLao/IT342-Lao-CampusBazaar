a721fcbf6489c0b0565bb58bc168845872aab9af
Mesha <mesha@Meshas-MacBook-Air.local>
2026-03-09 09:09:34 +0800

    Initial project structure - IT342 Campus Bazaar

---
+++ backend/campusbazaar/src/main/java/edu/cit/lao/campusbazaar/CampusbazaarApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.lao.campusbazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusbazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusbazaarApplication.class, args);
+	}
+
+}
