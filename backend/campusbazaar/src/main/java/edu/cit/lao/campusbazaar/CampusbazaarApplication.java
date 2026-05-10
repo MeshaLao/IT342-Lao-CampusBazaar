34f28527b3a98508a2c8d31a097a787f36b21528
MeshaLao <hilarydannelao@gmail.com>
2026-05-11 05:19:20 +0800

    Enable async processing with @EnableAsync

--- backend/campusbazaar/src/main/java/edu/cit/lao/campusbazaar/CampusbazaarApplication.java
+++ backend/campusbazaar/src/main/java/edu/cit/lao/campusbazaar/CampusbazaarApplication.java
@@ -2,12 +2,12 @@ package edu.cit.lao.campusbazaar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class CampusbazaarApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CampusbazaarApplication.class, args);
 	}
-
 }
